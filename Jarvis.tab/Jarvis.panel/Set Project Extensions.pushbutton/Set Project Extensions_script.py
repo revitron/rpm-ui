2435be601b30b0b161ee9feef4fdb780d6913d4c
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-22 17:19:47 +0200

    Update Set Project Extensions_script.py

--- Jarvis.tab/Jarvis.panel/Set Project Extensions.pushbutton/Set Project Extensions_script.py
+++ Jarvis.tab/Jarvis.panel/Set Project Extensions.pushbutton/Set Project Extensions_script.py
@@ -1,1 +1,7 @@
-import jarvis
+import revitron
+from revitron import _ 
+from pyrevit import script
+
+transaction = revitron.Transaction()
+_(revitron.DOC.ProjectInformation).set('Extensions', '', 'MultilineText')
+transaction.commit()
