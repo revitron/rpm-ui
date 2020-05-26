a230e7836a7f989a4a407cd5d241ff0d8cca403c
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-26 14:20:08 +0200

    Create Install Extensions_script.py

---
+++ Jarvis.tab/Jarvis.panel/Install Extensions.pushbutton/Install Extensions_script.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import revitron
+import jarvis
+import subprocess
+import os
+from rpw.ui.forms import FlexForm, TextBox, Button, Label
+from revitron import _
+
+if not revitron.Document.isFamily():
+
+    info = _(revitron.DOC.ProjectInformation)
+    lines = info.get('Extensions').split('\r\n')
+             
+    for line in lines:
+        
+        items = line.split('\t')
+        
+        try:
+            extType = items[0].strip()
+            extRepo = items[1].strip()
+            extName = os.path.basename(extRepo).replace('.git', '')
+            cmd = '{} extend {} {} {} --dest="{}"'.format(jarvis.JRVS_PYREVIT_BIN, extType, extName, extRepo, jarvis.JRVS_EXTENSIONS_DIR)
+            print('Installing {} "{}" from {} ...'.format(extType, extName, extRepo))
+        except:
+            pass
+        
+        
+        
