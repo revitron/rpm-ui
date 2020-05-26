fef12d977db2a521cb3547cf83be50c047fc620d
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-05-26 17:02:07 +0200

    Update Install Extensions_script.py

--- Jarvis.tab/Jarvis.panel/Install Extensions.pushbutton/Install Extensions_script.py
+++ Jarvis.tab/Jarvis.panel/Install Extensions.pushbutton/Install Extensions_script.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import revitron
 import jarvis
-import subprocess
 import os
+import glob
 from revitron import _
 
 if not revitron.Document.isFamily():
 
+    extManager = jarvis.ExtensionsManager()
+    extManager.removeAll()
+
     info = _(revitron.DOC.ProjectInformation)
-    lines = info.get('Extensions').split('\r\n')
-             
+    lines = info.get('Jarvis Extensions').split('\r\n')
+     
     for line in lines:
         
         items = line.split('\t')
@@ -18,10 +21,14 @@ if not revitron.Document.isFamily():
             extType = items[0].strip()
             extRepo = items[1].strip()
             extName = os.path.basename(extRepo).replace('.git', '')
-            cmd = '{} extend {} {} {} --dest="{}"'.format(jarvis.JRVS_PYREVIT_BIN, extType, extName, extRepo, jarvis.JRVS_EXTENSIONS_DIR)
-            print('Installing {} "{}" from {} ...'.format(extType, extName, extRepo))
+            print('Installing {}'.format(extName))
+            extManager.install(extName, extRepo, extType)
         except:
             pass
         
-        
-        
+    # Call pyRevit reload command to reload pyRevit
+    # The reason to call the command instead of using sessionmgr module
+    # to realod is that the repo has been just updatedm so all
+    # modules need to be re-imported again in a clean engine.
+    from pyrevit.loader.sessionmgr import execute_command
+    execute_command('pyrevitcore-pyrevit-pyrevit-tools-reload')
